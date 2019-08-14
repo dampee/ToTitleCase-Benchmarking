3cddae6de1427aa264743181ce2cf3b84995d982
Damiaan Peeters <damiaan@umbrace.be>
2019-08-14 19:02:43 +0200

    added more functions found on stackoverflow

--- TitleCaseBenchmark/Program.cs
+++ TitleCaseBenchmark/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
@@ -64,6 +66,101 @@ namespace TitleCaseBenchmark
             }
 
             return new string(str);
+        }
+
+        /// <summary>
+        /// Code from https://stackoverflow.com/a/38361008/97615
+        /// </summary>
+        /// <returns></returns>
+        [Benchmark]
+        public string WithSubString()
+        {
+            var tokens = TitleCase.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            for (var i = 0; i < tokens.Length; i++)
+            {
+                var token = tokens[i];
+                tokens[i] = token == token.ToUpper()
+                    ? token 
+                    : token.Substring(0, 1).ToUpper() + token.Substring(1).ToLower();
+            }
+
+            return string.Join(" ", tokens);
+        }
+
+        /// <summary>
+        /// Code From https://stackoverflow.com/a/40322543/97615
+        /// </summary>
+        /// <returns></returns>
+        [Benchmark]
+        public string WithRegexAndCultureInfo()
+        {
+            var tokens = Regex.Split(CultureInfo.TextInfo.ToLower(TitleCase), "([ -])");
+
+            for (var i = 0; i < tokens.Length; i++)
+            {
+                if (!Regex.IsMatch(tokens[i], "^[ -]$"))
+                {
+                    tokens[i] = $"{CultureInfo.TextInfo.ToUpper(tokens[i].Substring(0, 1))}{tokens[i].Substring(1)}";
+                }
+            }
+
+            return string.Join("", tokens);
+        }
+
+        /// <summary>
+        /// Code from https://stackoverflow.com/a/45113271/97615
+        /// Leaves acronyms
+        /// </summary>
+        /// <returns></returns>
+        [Benchmark]
+        public string WithCharsAndStringBuilderAndMore()
+        {
+            var tokens = TitleCase.Split(new[] { " " }, StringSplitOptions.None);
+            var stringBuilder = new StringBuilder();
+            for (var ti = 0; ti < tokens.Length; ti++)
+            {
+                var token = tokens[ti];
+                if (token == token.ToUpper())
+                    stringBuilder.Append(token + " ");
+                else
+                {
+                    var previousWasSeperator = false;
+                    var previousWasNumber = false;
+                    var ignoreNumber = false;
+                    for (var i = 0; i < token.Length; i++)
+                    {
+
+                        if (char.IsNumber(token[i]))
+                        {
+                            stringBuilder.Append(token[i]);
+                            previousWasNumber = true;
+                        }
+                        else if (!char.IsLetter(token[i]))
+                        {
+                            stringBuilder.Append(token[i]);
+                            previousWasSeperator = true;
+                        }
+                        else if ((previousWasNumber && !ignoreNumber) || previousWasSeperator)
+                        {
+                            stringBuilder.Append(char.ToUpper(token[i]));
+                            previousWasSeperator = false;
+                            previousWasNumber = false;
+                        }
+                        else if (i == 0)
+                        {
+                            ignoreNumber = true;
+                            stringBuilder.Append(char.ToUpper(token[i]));
+                        }
+                        else
+                        {
+                            ignoreNumber = true;
+                            stringBuilder.Append(char.ToLower(token[i]));
+                        }
+                    }
+                    stringBuilder.Append(" ");
+                }
+            }
+            return stringBuilder.ToString().TrimEnd();
         }
     }
 }
